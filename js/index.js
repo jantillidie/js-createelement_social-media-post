9baa1f45a921d849e5c2831d9e03d49e78afca7c
Jan Maaß <git@antipost.de>
2023-11-02 12:51:17 +0100

    Add solution

--- js/index.js
+++ js/index.js
@@ -10,3 +10,27 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const secondPost = document.createElement("article");
+secondPost.classList.add("post");
+
+const secondPostContent = document.createElement("p");
+secondPostContent.classList.add("post__content");
+secondPostContent.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+const secondPostFooter = document.createElement("footer");
+secondPostFooter.classList.add("post__footer");
+
+const secondPostSpan = document.createElement("span");
+secondPostSpan.classList.add("post__username");
+secondPostSpan.textContent = "@username";
+
+const secondPostButton = document.createElement("button");
+secondPostButton.classList.add("post__button");
+secondPostButton.textContent = "♥ Like";
+
+secondPostButton.addEventListener("click", handleLikeButtonClick);
+
+secondPostFooter.append(secondPostSpan, secondPostButton);
+secondPost.append(secondPostContent, secondPostFooter);
+document.body.append(secondPost);
